213219da614580820d3e8959775570b2ee48ace7
Erik Plachta <eplachta@gmail.com>
2021-11-17 20:03:10 -0500

    confirmed validating

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,56 @@
-/* Defining OOP
-
-    1. On load
-      - _build()
-        creates on click event listener for button
-
-    2. User clicks button with id #generate
-      - get_Password()
-          on click runs
-
-    3. User gets prompts
-
-    4. Password is generated in HTML
-      - set_Password()
-
-*/
-
-/*
-  1. On Load 
-      - GLOBAL VARIABLES
-*/
-
-
+//*--------------------------------------------------------------------------*//
+//-- 1. Grab el from HTML for JS
+    
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", get_GeneratedPassword);
 
 
-
-/*
-  2. User clicks on Generate Password
+//*--------------------------------------------------------------------------*//
+//-- 2. User clicks on Generate Password
     
-*/
 
 // Once Generated, write password to the #password input
 function get_GeneratedPassword() {
   // var password = _generatePassword();
-  var passwordText = document.querySelector("#password").value= _generatePassword();
+  var passwordText = document.querySelector("#password").value= generatePassword();
 
   // passwordText.value = password;
 }
 
-/* 
-  PRIVATE FUNCTIONS
-*/
+/*----------------------------------------------------------------------------*/
+//--  PRIVATE FUNCTIONS
 
-// Manages generating password
-function _generatePassword() {
+/*----------------------------------------------------------------------------*/
+//--  Manages generating password
+function generatePassword() {
   /* Reads user args, sends results */
   
-  // Made True if at least 1 checkbox is clicked
-  var decided = false; 
-  
+  //payload to html
+  results = ''; 
   // Get input 
-  var choices = _get_UserInput();
-  var choices_validated = _get_ValidationResults(choices);
+  var choices = get_UserInput();
+  // Made True if at least 1 checkbox is clicked
+  var choice_WasMade_bool = check_ChoiceWasMade(choices);
+  if (choice_WasMade_bool) {
+    if (check_PasswordLength(choices.choice_Length)) {
+      // results = "worked";
+      results = get_Password(choices);
+    }
+    else {
+      results = "User made a choice, but password length not within params.";
+    }
+  } else {
+    results = "User MUST select an option first.";
+  }
 
-  return choices;
+  return results;
 }
 
-// Reads user input
-function _get_UserInput(){
+/*----------------------------------------------------------------------------*/
+//-- Reads user input
+function get_UserInput(){
   // Object holding User Input results
 
   // Get's values from HTML Document and stores in object for validation
@@ -77,57 +67,73 @@ function _get_UserInput(){
       choice_Length : (document.querySelector("#choice_Length").value),
   };
   
-  // verify if choice was made
-  var $results = _get_ValidationResults(choices);
-
-  // GET the password generated
-  
-  //return password to HTML
-  return $results;
+  return choices;
 };
 
-function _get_ValidationResults(choices){
+/*----------------------------------------------------------------------------*/
+//--verify if eu made choice
+function check_ChoiceWasMade(choices){
   
-  var $results = [
-    //boolean variable to see if EU made choice.
-    choice_WasMade_bool = false,
-    // list holding key names for reference of what to generate from.
-    choices_Selected = ["test"],
-  ];
+  var choice_WasMade_bool = false; //boolean variable to see if EU made choice.
   
-  // Itterate thru keys in obj choices
   Object.keys(choices).forEach( function(key) {  
     // For everything except for length
-    //if (key != "choice_Length") {
-      // check to see if at least 1 value is selected
-      if (choices[key] != true){
-        // If YES, user made a choice so true
-        $results.choice_WasMade_bool = true;
-        // console.log(key, choices[key]);
-      } 
-      // else {
-      //   console.log(key, choices[key]);
-      // }
-      
-    });
-  
-  // Object.keys(choices).forEach( function(key) {
-  //   console.log(key, choices[key]);
-  // });
-
-  return $results;
+    if (key != "choice_Length") { 
+      // check to see if at least 1 value is selected   
+      if (choices[key].toString() == "true"){ // If TRUE, user made a choice so true
+        choice_WasMade_bool = true;
+        
+      }
+    };
+  });
+  return choice_WasMade_bool;
+};
+/*----------------------------------------------------------------------------*/
+//-- verify length in parameters
+function check_PasswordLength(choice_Length){
+  var min = 8;
+  var max = 128;
+  choice_Length_bool = false;
+  if (choice_Length >= min && choice_Length <= max){
+    choice_Length_bool = true;
+  }
+  return choice_Length_bool;
 };
 
-// Builds password based on user input
-function _set_Password(choices){
+
+/*----------------------------------------------------------------------------*/
+//-- Builds password based on user input
+function get_Password(choices){
   
+  //String holder to be returned as password
   var generatedPassword = '';
   
-  /* DEFINING LOCAL VARIABLES TO POPULATE PASSWORDS */
-  var char_AlphaLowercase = 'abcdefghijklmnopqrstuvwxyz';
-  var char_AlphaUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var char_Numeric = '0123456789';
-  var char_Special = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  // OBJ to hold potential characters for building password
+  var options = {
+    choice_AlphaLowercase : "abcdefghijklmnopqrstuvwxyz",
+    choice_AlphaUppercase : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    choice_Numeric : "0123456789",
+    choice_SpecialChar : " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+  }
 
+  // String holder for charactres based on EU's selection. ( To be split to array below)
+  var char_UserSelected = ""; 
+  Object.keys(choices).forEach( function(key) {
+    // if I need to pull values for spec characters
+    if(key in options){
+      // and user requested it
+      if(choices[key]){
+        // build string to pick from
+        char_UserSelected = char_UserSelected +options[key];
+      }
+    }
+  });
+  
+  for (var i=0; i < choices.choice_Length; i++){
+    generatedPassword = generatedPassword + (char_UserSelected[Math.floor(Math.random() * char_UserSelected.length)]);
+  }
+  
+  
   return generatedPassword;
-}
+};
+
